906280320ba7a21526d9348198ce6dd6a5737a42
Juan Alberto Camacho Bolanos <juancamacho@ciencias.unam.mx>
2021-02-19 13:42:27 -0600

    Agrega los elementos del menú en el toolbar

--- app/src/main/java/com/example/proyectodemdulo/MainActivity.kt
+++ app/src/main/java/com/example/proyectodemdulo/MainActivity.kt
@@ -1,11 +1,48 @@
 package com.example.proyectodemdulo
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        // Agrega los elementos del menu si el action bar esta presente
+        menuInflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // Manejamos el evento de dar clic en el action bar
+        val id = when(item.itemId) {
+            R.id.primer_accion -> {
+                Toast.makeText(this, "Menú 1", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.segunda_accion -> {
+                Toast.makeText(this, "Menú 2", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.tercer_accion -> {
+                Toast.makeText(this, "Menú 3", Toast.LENGTH_LONG).show()
+                return true
+            }
+            R.id.cuarta_accion -> {
+                Toast.makeText(this, "Menú 4", Toast.LENGTH_LONG).show()
+                return true
+            }
+            else -> {
+                Toast.makeText(this, "Menú 5", Toast.LENGTH_LONG)
+                return true
+            }
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
